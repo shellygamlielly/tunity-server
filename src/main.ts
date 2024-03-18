c1d96ed7cc6dff92251aa822092f0eae217e59fd
Shelly Gamlielly <shellygamlielly@gmail.com>
2024-03-18 14:38:13 +0200

    serverlessExpress, webpack (boundle all) ,serverless offline for dev, mongo is used once on startup,

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverlessExpress from 'aws-serverless-express';
 
-async function bootstrap() {
-  const expressApp = express();
-  // adapter that converts Lambda events into HTTP requests.
-  const adapter = new ExpressAdapter(expressApp);
-  const app = await NestFactory.create(AppModule, adapter);
+let server: Handler;
 
-  app.useGlobalPipes(new ValidationPipe());
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
 
-  app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET, POST, PUT, DELETE',
-    credentials: true,
-  });
-
-  return app;
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 
-async function init() {
-  if (process.env.NODE_ENV === 'development') {
-    const app = await bootstrap();
-    const config = new DocumentBuilder()
-      .setTitle('Playlist example')
-      .setDescription('The playlist API description')
-      .setVersion('1.0')
-      .addTag('Tunity')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-    await app.listen(3000);
-  }
-}
-init();
-
-// For deployment to AWS Lambda
-export const handler = async (event, context) => {
-  const app = await bootstrap();
-  const server = serverlessExpress.createServer(app);
-  return serverlessExpress.proxy(server, event, context);
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
 };
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = (options, webpack) => {
+  const lazyImports = [
+    '@nestjs/microservices/microservices-module',
+    '@nestjs/websockets/socket-module',
+    'class-transformer/storage', // https://github.com/nestjs/mapped-types/issues/486#issuecomment-932715880
+  ];
+
+  return {
+    ...options,
+    externals: [],
+    output: {
+      ...options.output,
+      libraryTarget: 'commonjs2',
+    },
+    plugins: [
+      ...options.plugins,
+      new webpack.IgnorePlugin({
+        checkResource(resource) {
+          if (lazyImports.includes(resource)) {
+            try {
+              require.resolve(resource);
+            } catch (err) {
+              return true;
+            }
+          }
+          return false;
+        },
+      }),
+    ],
+  };
+};
