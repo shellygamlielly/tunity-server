df0c10ee56fadfbd7103b52380fc05a50aab9a09
Shelly Gamlielly <shellygamlielly@gmail.com>
2024-03-21 13:53:23 +0200

    if development mode the handler returns null and local bootstrap is called

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@codegenie/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
-
 let server: Handler;
 
-async function bootstrap(): Promise<Handler> {
+async function bootstrap_aws(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
+async function bootstrap_local() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+}
 
+if (process.env.NODE_ENV === 'development') {
+  bootstrap_local();
+}
 export const handler: Handler = async (
   event: any,
   context: Context,
   callback: Callback,
 ) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
+  if (process.env.NODE_ENV !== 'development') {
+    server = server ?? (await bootstrap_aws());
+    return server(event, context, callback);
+  }
 };
